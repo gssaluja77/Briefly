2a9f25ee1ee26b68fffb84818992ec235bf55dae
Gundeep Singh Saluja <gssaluja77@gmail.com>
2025-11-13 02:01:20 -0800

    Removed loading spinner component

--- src/components/LoadingSpinner.jsx
+++
@@ -1,7 +0,0 @@
-export default function LoadingSpinner() {
-  return (
-    <div className="flex flex-col items-start justify-center">
-      <div className="w-6 h-6 border-3 border-dashed rounded-full animate-spin border-white"></div>
-    </div>
-  );
-}
